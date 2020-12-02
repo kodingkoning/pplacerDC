882f19ecec53fc121ef2683c5f1740ca54d7d9ab
Malachi Phillips <malachi2@illinois.edu>
2020-12-01 19:59:15 -0600

    Update pplacer+APPLES results

--- data/vs-data/mvdat.sh
+++ data/vs-data/mvdat.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-for size in 500 1000 5000 10000 50000; do
+for size in 500 1000 5000 10000; do
   cd $size
-  cp /home/malachi2/cs581-project/runs/variable-size/data/$size/delta_error_approach1.txt .
-  cp /home/malachi2/cs581-project/runs/variable-size/data/$size/time_approach1.txt .
-  cp /home/malachi2/cs581-project/runs/variable-size/data/$size/run_output.log .
+  #cp /home/malachi2/cs581-project/runs/variable-size/data/$size/delta_error_approach1.txt .
+  #cp /home/malachi2/cs581-project/runs/variable-size/data/$size/time_approach1.txt .
+  #cp /home/malachi2/cs581-project/runs/variable-size/data/$size/run_output.log .
 
   # stuff for approach 2
   cp /home/malachi2/cs581-project/runs/variable-size/data/$size/delta_error_approach2.txt .
