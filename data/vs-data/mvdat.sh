7800479f482ee276ab3eb4403a1691a71624d1bf
Malachi Phillips <malachi2@illinois.edu>
2020-12-03 09:00:12 -0600

    Update results

--- data/vs-data/mvdat.sh
+++ data/vs-data/mvdat.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 for size in 500 1000 5000 10000 50000; do
   cd $size
-  #cp /home/malachi2/cs581-project/runs/variable-size/data/$size/delta_error_approach1.txt .
-  #cp /home/malachi2/cs581-project/runs/variable-size/data/$size/time_approach1.txt .
-  #cp /home/malachi2/cs581-project/runs/variable-size/data/$size/run_output.log .
+  cp /home/malachi2/cs581-project/runs/variable-size/data/$size/delta_error_approach1.txt .
+  cp /home/malachi2/cs581-project/runs/variable-size/data/$size/time_approach1.txt .
+  cp /home/malachi2/cs581-project/runs/variable-size/data/$size/run_output.log .
 
   # stuff for approach 2
-  cp /home/malachi2/cs581-project/runs/variable-size/data/$size/delta_error_approach2.txt .
-  cp /home/malachi2/cs581-project/runs/variable-size/data/$size/delta_error_apples.txt .
-  cp /home/malachi2/cs581-project/runs/variable-size/data/$size/time_approach2.txt .
-  cp /home/malachi2/cs581-project/runs/variable-size/data/$size/run_output2.log .
+  #cp /home/malachi2/cs581-project/runs/variable-size/data/$size/delta_error_approach2.txt .
+  #cp /home/malachi2/cs581-project/runs/variable-size/data/$size/delta_error_apples.txt .
+  #cp /home/malachi2/cs581-project/runs/variable-size/data/$size/time_approach2.txt .
+  #cp /home/malachi2/cs581-project/runs/variable-size/data/$size/run_output2.log .
   cd ../
 done
