5de1e5a1208c0a52159307a839336546f2bec1f6
kodingkoning <koningelizabeth@gmail.com>
2020-12-07 20:39:19 -0600

    added 2500 problem size to vary-size graph

--- data/varying_size_error_plots.py
+++ data/varying_size_error_plots.py
@@ -14,9 +14,12 @@ testedSizes = [100, 500, 1000]
 data=[]
 for size in testedSizes:
   data.append(get_errs(f"approach1_size_{size}", problemSize))
+data.append(np.sort(read_list("BW-results/5000/delta_error_approach1.txt")))
+# TODO: append data for 2500
 fig, ax = plt.subplots()
 ax.boxplot(data)
-ax.set_xticklabels(testedSizes)
+labels = [100, 500, 1000, 2500]
+ax.set_xticklabels(labels)
 plt.title(f"Delta Error for DC-pplacer v. Subproblem Size")
 plt.ylabel("Delta Error")
 plt.xlabel("Subproblem Size")
